6d09aa64c824f07743dccea6da64a6a8e6ff5c4c
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2025-01-16 19:18:46 +0000

    reviewed and updated institution.index router

--- src/server/routers/institution/index.ts
+++ src/server/routers/institution/index.ts
@@ -1,69 +1,68 @@
-import { AdminLevel } from "@prisma/client";
 import { z } from "zod";
 
-import { slugify } from "@/lib/utils/general/slugify";
-import { groupParamsSchema } from "@/lib/validations/params";
+import { createTRPCRouter } from "@/server/trpc";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  superAdminProcedure,
-} from "@/server/trpc";
-import { isSuperAdmin } from "@/server/utils/admin/is-super-admin";
-
+import { AllocationGroup } from "@/data-objects/spaces/group";
+import { groupDtoSchema, userDtoSchema } from "@/dto";
+import { procedure } from "@/server/middleware";
 import { groupRouter } from "./group";
 import { instanceRouter } from "./instance";
 import { subGroupRouter } from "./sub-group";
 
+// TODO @pkitazos please review
 export const institutionRouter = createTRPCRouter({
   group: groupRouter,
   subGroup: subGroupRouter,
   instance: instanceRouter,
 
-  superAdminAccess: protectedProcedure.query(async ({ ctx }) => {
-    const access = await isSuperAdmin(ctx.db, ctx.session.user.id);
-    return access;
-  }),
+  superAdminAccess: procedure.user
+    .output(z.boolean())
+    .query(async ({ ctx: { user } }) => await user.isSuperAdmin()),
 
-  groupManagement: superAdminProcedure.query(async ({ ctx }) => {
-    const groups = await ctx.db.allocationGroup.findMany({});
-    const superAdmins = await ctx.db.adminInSpace.findMany({
-      where: { adminLevel: AdminLevel.SUPER },
-      select: { user: { select: { id: true, name: true, email: true } } },
-    });
-    return { groups, superAdmins: superAdmins.map(({ user }) => user) };
-  }),
-
-  takenGroupNames: superAdminProcedure.query(async ({ ctx }) => {
-    const groups = await ctx.db.allocationGroup.findMany({
-      select: { displayName: true },
-    });
-    return new Set(groups.map(({ displayName }) => displayName));
-  }),
-
-  createGroup: superAdminProcedure
-    .input(z.object({ groupName: z.string() }))
-    .mutation(async ({ ctx, input: { groupName } }) => {
-      await ctx.db.allocationGroup.create({
-        data: {
-          id: slugify(groupName),
-          displayName: groupName,
-        },
-      });
+  // slightly odd one
+  // Consider splitting into two?
+  // or perhaps just renaming? what does this really do?
+  groupManagement: procedure.superAdmin
+    // Best guess at output - need ur input @pkitazos
+    .output(
+      z.object({
+        groups: z.array(groupDtoSchema),
+        superAdmins: z.array(userDtoSchema),
+      }),
+    )
+    .query(async ({ ctx: { dal } }) => {
+      const groups = await dal.group.getAll();
+      const superAdmins = await dal.superAdmin.getAll();
+      return { groups, superAdmins };
     }),
 
-  deleteGroup: superAdminProcedure
-    .input(z.object({ params: groupParamsSchema }))
-    .mutation(
-      async ({
-        ctx,
-        input: {
-          params: { group },
-        },
-      }) => {
-        await ctx.db.allocationGroup.delete({
-          where: { id: group },
-        });
-      },
+  // superAdminProcedure.query(async ({ ctx }) => {
+  //   const groups = await ctx.db.allocationGroup.findMany({});
+  //   const superAdmins = await ctx.db.adminInSpace.findMany({
+  //     where: { adminLevel: AdminLevel.SUPER },
+  //     select: { user: { select: { id: true, name: true, email: true } } },
+  //   });
+  //   return { groups, superAdmins: superAdmins.map(({ user }) => user) };
+  // }),
+
+  takenGroupNames: procedure.superAdmin
+    .output(z.set(z.string()))
+    .query(
+      async ({ ctx: { dal } }) =>
+        new Set((await dal.group.getAll()).map((x) => x.displayName)),
     ),
+
+  createGroup: procedure.superAdmin
+    .input(z.object({ groupName: z.string() }))
+    .output(groupDtoSchema) // used to be z.void(); still should be safe.
+    .mutation(
+      async ({ ctx: { dal }, input: { groupName } }) =>
+        await AllocationGroup.create(dal, groupName),
+    ),
+
+  deleteGroup: procedure.group.superAdmin
+    .output(z.void())
+    .mutation(async ({ ctx: { group } }) => {
+      await group.delete();
+    }),
 });
