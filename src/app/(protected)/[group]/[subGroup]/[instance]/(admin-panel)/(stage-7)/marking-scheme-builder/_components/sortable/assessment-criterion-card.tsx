6378166dfe06400477f2d8ff0b84a31d79272d6c
Petros <93052465+pkitazos@users.noreply.github.com>
2025-02-10 12:08:20 +0000

    UI scaffolding for marking scheme builder form

--- src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-7)/marking-scheme-builder/_components/sortable/assessment-criterion-card.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-7)/marking-scheme-builder/_components/sortable/assessment-criterion-card.tsx
@@ -4,16 +4,21 @@ import { CSS } from "@dnd-kit/utilities";
 import { GripVerticalIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 import { cn } from "@/lib/utils";
 
 import { AssessmentCriterion } from "../state/store";
 
+import { AssessmentCriterionForm } from "./assessment-criterion-form";
+import { FormDivider } from "./form-divider";
+
 export function AssessmentCriterionCard({
   item,
+  handleNewCriterion,
 }: {
   item: AssessmentCriterion;
+  handleNewCriterion: () => void;
 }) {
   const {
     setNodeRef,
@@ -41,36 +46,32 @@ export function AssessmentCriterionCard({
   }
 
   return (
-    <Card
-      ref={setNodeRef}
-      style={style}
-      className={cn(
-        "group flex items-center",
-        isOver && "outline outline-4 outline-muted-foreground/50",
-      )}
-    >
-      <Button
-        {...attributes}
-        {...listeners}
-        variant="ghost"
-        className="ml-2 flex h-20 w-8 flex-col"
+    <>
+      <Card
+        ref={setNodeRef}
+        style={style}
+        className={cn(
+          "group flex items-center pt-5",
+          isOver && "outline outline-4 outline-muted-foreground/50",
+        )}
       >
-        <GripVerticalIcon className="-mb-1 h-8 w-8 cursor-move text-gray-400/50" />
-        <GripVerticalIcon className="h-8 w-8 cursor-move text-gray-400/50" />
-      </Button>
+        <Button
+          {...attributes}
+          {...listeners}
+          variant="ghost"
+          className="ml-2 flex h-20 w-8 flex-col"
+        >
+          <GripVerticalIcon className="-mb-1 h-8 w-8 cursor-move text-gray-400/50" />
+          <GripVerticalIcon className="h-8 w-8 cursor-move text-gray-400/50" />
+        </Button>
 
-      <div>
-        <CardHeader className="flex w-full flex-row items-center justify-between space-y-0 p-4 pb-2">
-          <CardTitle className="text-sm font-medium">{item.name}</CardTitle>
-        </CardHeader>
-        <CardContent className="pl-8">
-          <div className="flex items-center">
-            <span className="font-medium">{item.description}</span>
-            <span className="font-medium">weight: {item.weight}</span>
-            <span className="font-medium">rank: {item.rank}</span>
-          </div>
-        </CardContent>
-      </div>
-    </Card>
+        <div>
+          <CardContent className="pl-8">
+            <AssessmentCriterionForm defaultValues={item} />
+          </CardContent>
+        </div>
+      </Card>
+      <FormDivider onClick={handleNewCriterion} />
+    </>
   );
 }
