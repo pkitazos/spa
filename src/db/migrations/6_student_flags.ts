4261256b3fbf7e4078ff7dc5fdabe76c7dac5d45
Petros <93052465+pkitazos@users.noreply.github.com>
2025-03-11 21:55:58 +0000

    minor change to migration script

--- src/db/migrations/6_student_flags.ts
+++ src/db/migrations/6_student_flags.ts
@@ -2,31 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 import { expand } from "@/lib/utils/general/instance-params";
 import { InstanceParams } from "@/lib/validations/params";
+import studentFlags from "./data/FlagOnStudent.json";
 
 export async function student_flags(db: PrismaClient, params: InstanceParams) {
-  await db.$transaction(async (tx) => {
-    const flags = await tx.flag.findMany({ where: expand(params) });
-
-    const flagMap = flags.reduce(
-      (acc, val) => {
-        if (val.title === "Software Engineering") return acc;
-        const flagNum = parseInt(val.title.split("Level ")[1]);
-        return { ...acc, [flagNum]: val.id };
-      },
-      {} as Record<number, string>,
-    );
-
-    const students = await tx.studentDetails.findMany({
-      where: expand(params),
-    });
-
-    await tx.flagOnStudent.createMany({
-      data: students.map((s) => ({
-        ...expand(params),
-        studentId: s.userId,
-        flagId: flagMap[s.studentLevel],
-      })),
-      skipDuplicates: true,
-    });
+  await db.flagOnStudent.createMany({
+    data: studentFlags.map((s) => ({
+      ...expand(params),
+      studentId: s.studentId,
+      flagId: s.flagId,
+    })),
+    skipDuplicates: true,
   });
 }
