c5af04c18bb3a0c931f066f6d226f55d61da9b17
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2025-06-03 01:08:07 +0100

    Revert "testing staging deploy mechanism"
    This reverts commit 4d0d6250c3f03be1ee84290301a911bfcf1fbd5b.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
 
+import { Separator } from "@/components/ui/separator";
+import { UserInstances } from "@/components/user-instances";
+
 import { auth } from "@/lib/auth";
 
 import { app, metadataTitle } from "@/config/meta";
@@ -13,7 +16,24 @@ export default async function Home() {
 
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center gap-6">
-      <h1>Hello! this is a test!</h1>
+      <h1 className="flex flex-col items-center gap-3 font-medium">
+        <p className="text-4xl">
+          Welcome{" "}
+          <span className="font-mono font-semibold tracking-tighter text-indigo-600">
+            {user.name}
+          </span>
+          !
+        </p>
+        <p className="text-3xl text-slate-500">
+          to the SoCS project allocation system
+        </p>
+      </h1>
+      <Separator className="my-4 w-1/3" />
+      {user && (
+        <div className="absolute bottom-0 w-full max-w-5xl">
+          <UserInstances />
+        </div>
+      )}
     </div>
   );
 }
