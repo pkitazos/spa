9b5828ed035c3a6b1e1fd1967c19006bbea3d16e
Petros <93052465+pkitazos@users.noreply.github.com>
2025-06-15 22:35:52 +0200

    nuke scripts

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ import createMDX from "@next/mdx";
 const nextConfig = {
   output: "standalone",
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  webpack: (config) => {
-    config.module.rules.push({ test: /\.ts$/, exclude: /scripts\/db\// });
-    return config;
-  },
 };
 
 const withMDX = createMDX({ extension: /\.(md|mdx)$/ });
