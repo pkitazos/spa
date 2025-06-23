10b0c7bbdcb7f46c18a3deb11c45e1889a1f5cd0
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2025-06-23 17:30:12 +0100

    integrated new email stuff

--- scripts/deploy/commands/start.sh
+++ scripts/deploy/commands/start.sh
@@ -4,4 +4,12 @@ pushd $AMPS_LOC
 
 docker compose --env-file=./.env -f "./docker/docker-compose.$AMPS_DEP_MODE.yml" up -d
 
+if [[ $1 == 'email' ]]; then
+    if [ -z $AMPS_DEV_EMAILS ]; then
+        echo "Started $AMPS_DEP_MODE" | mutt -s "AMPS $AMPS_DEP_MODE: started" "$AMPS_DEV_EMAILS"
+    else 
+        echo "Dev emails not set"
+    fi
+fi
+
 popd
