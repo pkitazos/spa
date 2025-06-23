9720b8afffd17eaa2cb290cbace0f391a45eb716
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2025-06-23 17:30:12 +0100

    integrated new email stuff

--- scripts/deploy/commands/pull.sh
+++ scripts/deploy/commands/pull.sh
@@ -7,4 +7,12 @@ docker compose --env-file=./.env -f "./docker/docker-compose.$AMPS_DEP_MODE.yml"
 
 docker image prune -f
 
+if [[ $1 == 'email' ]]; then
+    if [ -z $AMPS_DEV_EMAILS ]; then
+        echo "Pulled and restarted $AMPS_DEP_MODE" | mutt -s "AMPS $AMPS_DEP_MODE: pulled" "$AMPS_DEV_EMAILS"
+    else 
+        echo "Dev emails not set"
+    fi
+fi
+
 popd
