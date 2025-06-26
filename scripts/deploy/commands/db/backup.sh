417535c8015aefd8587c8d75b2c901318698dc8e
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2025-06-26 14:28:28 +0100

    added db script re-writes

---
+++ scripts/deploy/commands/db/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pushd $AMPS_BACKUP_DIR
+
+outfile=db-image-$(date --iso-8601=seconds).sql
+
+docker exec amps-db-1 pg_dump -d ${DB_NAME} >$outfile
+
+gzip $outfile
+
+if [ ! -z $AMPS_CLI_GIT ]; then
+    git add $outfile
+    git commit -m "backup - $timestamp"
+    git push
+fi
+
+popd
---
+++ scripts/deploy/commands/db/prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pushd $AMPS_BACKUP_DIR
+
+LATEST_BACKUP=$(ls -t *.gz | head -n 1)
+
+if [ ! -z $LATEST_BACKUP ]; then
+    echo "Error: No .gz backup files found in '$AMPS_BACKUP_DIR'."
+    exit 1
+fi
+
+cp "$LATEST_BACKUP" "~"
+
+popd
