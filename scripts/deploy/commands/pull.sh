acb6e34175c28f5eccccad952de25ecb56b3c568
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2025-06-26 14:23:38 +0100

    Updated scripts to use flags/env vars rather than parsing separate args

--- scripts/deploy/commands/pull.sh
+++ scripts/deploy/commands/pull.sh
@@ -7,12 +7,13 @@ docker compose --env-file=./.env -f "./docker/docker-compose.$AMPS_DEP_MODE.yml"
 
 docker image prune -f
 
-if [[ $1 == 'email' ]]; then
+if [ ! -z $AMPS_CLI_EMAIL ]; then
     if [ -z $AMPS_DEV_EMAILS ]; then
-        echo "Pulled and restarted $AMPS_DEP_MODE" | mutt -s "AMPS $AMPS_DEP_MODE: pulled" "$AMPS_DEV_EMAILS"
-    else 
-        echo "Dev emails not set"
+        echo "Dev emails not specified"
+        exit 1
     fi
+    echo "Pulled and restarted $AMPS_DEP_MODE" |
+        mutt -s "AMPS $AMPS_DEP_MODE: pulled" "$AMPS_DEV_EMAILS"
 fi
 
 popd
