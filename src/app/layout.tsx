d9e75fed439de7f829dca375ee048e9a8cbe45fa
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2025-07-14 22:05:41 +0100

    Added nuqs Also updated next to 14.2 - this is a non-breaking change.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Toaster } from "sonner";
 
 import { Breadcrumbs } from "@/components/breadcrumbs";
@@ -25,24 +26,26 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning data-color-mode="light">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          forcedTheme="light"
-          disableTransitionOnChange
-        >
-          <TRPCReactProvider>
-            <Header />
-            <main className="flex h-[92dvh] w-full flex-col justify-start gap-4 bg-background">
-              <div className="ml-20 mt-7 flex items-center">
-                <Breadcrumbs />
-              </div>
-              <section className="mx-auto mt-1.5 flex h-full w-full justify-center 3xl:max-w-9xl">
-                {children}
-              </section>
-            </main>
-            <Toaster position="bottom-right" />
-          </TRPCReactProvider>
-        </ThemeProvider>
+        <NuqsAdapter>
+          <ThemeProvider
+            attribute="class"
+            forcedTheme="light"
+            disableTransitionOnChange
+          >
+            <TRPCReactProvider>
+              <Header />
+              <main className="flex h-[92dvh] w-full flex-col justify-start gap-4 bg-background">
+                <div className="ml-20 mt-7 flex items-center">
+                  <Breadcrumbs />
+                </div>
+                <section className="mx-auto mt-1.5 flex h-full w-full justify-center 3xl:max-w-9xl">
+                  {children}
+                </section>
+              </main>
+              <Toaster position="bottom-right" />
+            </TRPCReactProvider>
+          </ThemeProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
