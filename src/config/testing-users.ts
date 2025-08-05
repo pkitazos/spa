008c560f1ac3b93539194200f5108432d01362d7
Petros <93052465+pkitazos@users.noreply.github.com>
2025-08-05 16:25:51 +0300

    updated test users

--- src/config/testing-users.ts
+++ src/config/testing-users.ts
@@ -5,7 +5,8 @@ import { Role } from "@/db/types";
  */
 export const testUserEmails = [
   { ord: 0, email: "petros.kitazos@glasgow.ac.uk", roles: [Role.ADMIN] },
-  { ord: 1, email: "william.pettersson@glasgow.ac.uk", roles: [Role.ADMIN] },
+  { ord: 0, email: "j.trevor.1@research.gla.ac.uk", roles: [Role.ADMIN] },
+  { ord: 1, email: "johnh.williamson@glasgow.ac.uk", roles: [Role.ADMIN] },
   {
     ord: 2,
     email: "yiannis.giannakopoulos@glasgow.ac.uk",
@@ -22,6 +23,6 @@ export const testUserEmails = [
     email: "david.manlove@glasgow.ac.uk",
     roles: [Role.SUPERVISOR, Role.READER],
   },
-  { ord: 6, email: "Craig.Macdonald@glasgow.ac.uk", roles: [Role.READER] },
+  { ord: 6, email: "craig.macdonald@glasgow.ac.uk", roles: [Role.READER] },
   { ord: 7, email: "2084268d@student.gla.ac.uk", roles: [Role.STUDENT] },
 ];
