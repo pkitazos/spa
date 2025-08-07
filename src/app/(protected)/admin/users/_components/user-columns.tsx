22f024e9d6b42e15a4a561847cf72a2f5f5555a8
Petros <93052465+pkitazos@users.noreply.github.com>
2025-08-07 18:09:14 +0300

    wip: added user info page and edit form

--- src/app/(protected)/admin/users/_components/user-columns.tsx
+++ src/app/(protected)/admin/users/_components/user-columns.tsx
@@ -1,92 +1,75 @@
 "use client";
 
-import { DialogTrigger } from "@radix-ui/react-dialog";
 import { type ColumnDef } from "@tanstack/react-table";
-import { PenIcon, Trash2Icon } from "lucide-react";
+import { EditIcon, Trash2Icon } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 import { INSTITUTION } from "@/config/institution";
+import { PAGES } from "@/config/pages";
 
 import { type UserDTO } from "@/dto";
 
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { WithTooltip } from "@/components/ui/tooltip-wrapper";
 import { YesNoAction } from "@/components/yes-no-action";
 
 import { api } from "@/lib/trpc/client";
+import { cn } from "@/lib/utils";
 
-export const columns = [
-  { id: "id", accessorKey: "id", header: INSTITUTION.ID_NAME },
-  { id: "name", accessorKey: "name", header: "Name" },
-  { id: "email", accessorKey: "email", header: "Email" },
-  {
-    id: "actions",
-    header: "Actions",
-    cell: ({ row }) => {
-      const { mutateAsync: api_delete } = api.user.deleteById.useMutation();
+export const useUserManagementColumns = () => {
+  const router = useRouter();
 
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const router = useRouter();
+  return [
+    { id: "id", accessorKey: "id", header: INSTITUTION.ID_NAME },
+    { id: "name", accessorKey: "name", header: "Name" },
+    { id: "email", accessorKey: "email", header: "Email" },
+    {
+      id: "actions",
+      header: "Actions",
+      cell: ({ row }) => {
+        // ! this is not how we do things bro
+        const { mutateAsync: api_delete } = api.user.deleteById.useMutation();
 
-      return (
-        <div className="flex w-14 items-center justify-center gap-2">
-          <YesNoAction
-            title="Deleted User"
-            description={`You're about to delete ${row.original.name} (${row.original.id}) from the system. This is permanent and cannot be undone. Are you sure you wish to proceed?`}
-            action={() =>
-              void toast
-                .promise(api_delete({ userId: row.original.id }), {
-                  loading: "Deleting User...",
-                  success: (data) =>
-                    `Successfully deleted user ${data.name} (${data.id})`,
-                  error: `Failed to delete user`,
-                })
-                .unwrap()
-                .then(() => router.refresh())
-            }
-            trigger={
-              <WithTooltip tip="Delete user">
-                <Button variant="destructive" className="flex items-center">
-                  <Trash2Icon className="h-4 w-4" />
-                </Button>
-              </WithTooltip>
-            }
-          />
-          <Dialog>
-            <DialogTrigger>
-              <WithTooltip tip="Edit user details">
-                <Button>
-                  <PenIcon className="h-4 w-4" />
-                </Button>
-              </WithTooltip>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Edit user details</DialogTitle>
-                <DialogDescription></DialogDescription>
-              </DialogHeader>
-              <EditUserDetailsForm user={row.original} />
-              <DialogFooter className="sm:justify-start">
-                <DialogClose asChild>
-                  <Button type="button" variant="secondary">
-                    Close
+        const user = row.original;
+
+        return (
+          <div className="flex w-14 items-center justify-center gap-2">
+            <YesNoAction
+              title="Deleted User"
+              description={`You're about to delete ${user.name} (${user.id}) from the system. This is permanent and cannot be undone. Are you sure you wish to proceed?`}
+              action={() =>
+                void toast
+                  .promise(api_delete({ userId: user.id }), {
+                    loading: "Deleting User...",
+                    success: (data) =>
+                      `Successfully deleted user ${data.name} (${data.id})`,
+                    error: `Failed to delete user`,
+                  })
+                  .unwrap()
+                  .then(() => router.refresh())
+              }
+              trigger={
+                <WithTooltip tip="Delete user">
+                  <Button variant="destructive" className="w-10 h-10">
+                    <Trash2Icon className="h-4 w-4" />
                   </Button>
-                </DialogClose>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        </div>
-      );
+                </WithTooltip>
+              }
+            />
+            <Link
+              className={cn(
+                buttonVariants({ variant: "outline", size: "sm" }),
+                "w-10 h-10",
+              )}
+              href={`/${PAGES.superAdminPanel.href}/${PAGES.userManagement.href}/${user.id}`}
+            >
+              <EditIcon className="h-4 w-4" />
+            </Link>
+          </div>
+        );
+      },
     },
-  },
-] satisfies ColumnDef<UserDTO>[];
+  ] satisfies ColumnDef<UserDTO>[];
+};
